076b4fe224ab0ff87429991b5464435a4e9392b7
Mushfiqur Rahman <mushfiqur.rahman22.12@gmail.com>
2023-04-24 14:33:02 -0700

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+      $name = "Mushfiqur Rahman";
+      $ID = 2121535642;
+      $CGPA = 3.46;
+
+      echo "<h2>Student Name: $name</h2>";
+      echo "<br>";
+      echo "<br>Student ID: $ID</br>";
+      echo "<br>";
+      echo "<br>Student's CGPA: $CGPA</br>";
+
+     ?>
+  </body>
+</html>
