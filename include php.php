9be972167e5a87345b214a44c623ec83658e002e
Mushfiqur Rahman <mushfiqur.rahman22.12@gmail.com>
2023-04-27 15:04:19 -0700

    Add files via upload

---
+++ include php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $title="Discrete Mathematics";
+      $author="Anton Davis";
+      $wordcount=4000;
+      include "article header.php";
+      echo "<br>$meter<br>";
+      intro("Mushfiq");
+    ?>
+  </body>
+</html>
