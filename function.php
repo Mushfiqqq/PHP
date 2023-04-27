1dc00b3beb2fb0cf9ad5a09fa3be36febc947f8a
Mushfiqur Rahman <mushfiqur.rahman22.12@gmail.com>
2023-04-27 00:33:30 -0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      function sayHi($name, $age){
+        echo "Hello $name, you are $age<br>";
+      }
+
+      sayHi("mushfiq", 22);
+      sayHi("sami", 21);
+      sayhi("nourin", 20);
+    ?>
+  </body>
+</html>
