cefe3ed7243258c96bf61ed78939957ef3162292
Mushfiqur Rahman <mushfiqur.rahman22.12@gmail.com>
2023-04-27 14:42:00 -0700

    Add files via upload

---
+++ include html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php include "header.html" ?>
+    <p>My name is Mushfiqur Rahman</p>
+    <?php include "footer.html" ?>
+  </body>
+</html>
