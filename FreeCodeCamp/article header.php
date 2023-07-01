6d8c95de1550dc1f526f55b9dae10d0d0bb91ab7
Mushfiqur Rahman <mushfiqur.rahman22.12@gmail.com>
2023-07-01 20:53:50 +0600

    article header.php

--- FreeCodeCamp/article header.php
+++ FreeCodeCamp/article header.php
@@ -1,1 +1,10 @@
+<h2> <?php echo $title; ?> </h2>
+<h4> <?php echo $author; ?> </h4>
+Word Count: <?php echo $wordcount; ?>
 
+<?php
+  $meter=100;
+  function intro($name){
+    echo "Hello $name";
+  }
+?>
